6820533f0aaca17ad32aacec99052433d00149f6
IsaVip <isa.depaulapereira@hotmal.com>
2024-04-11 15:26:51 -0300

    teste

---
+++ app.js
@@ -0,0 +1,74 @@
+/*Questões dia 04/04/2024
+
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do desafio';
+
+function exibirTextoNoConsole() {
+    console.log ("O botão foi clicado!");
+}
+
+function mensagemAlert() {
+    alert ("Eu amo JS");
+}
+
+let cidade;
+function informaçãoPrompt() {
+    cidade = prompt("Escolha o nome de uma cidade do Brasil");
+    alert (`Estive em ${cidade} e lembrei de você`);
+}
+
+
+function botãoSoma() {
+    let número1 = parseInt(prompt ("Escolha o primeiro número da soma"));
+    let número2 = parseInt(prompt ("Agora, escolha o segundo"));
+    let resultado = (número1+número2);
+    alert (`O resultado da sua conta é: ${resultado}`);
+}*/
+
+//Questões dia 05/04/2024
+
+// function exibirTextoNoConsole() {
+//      console.log ('Olá, Mundo!');
+// }
+
+// function exibirTextoNoConsole(texto){
+//      let nome = prompt ('Digite aqui seu nome');
+//      console.log (`Olá, ${nome}`);
+// } 
+
+// n = parseInt(prompt("Digite um numero"));
+// let resultado = Conta(n);
+// function Conta(num){
+//      return (num*2);
+// }
+// alert (`O dobro de ${n} é ${resultado}`);
+
+
+// let n1 = parseInt(prompt ('Digite aqui o primeiro numero'));
+// let n2 = parseInt(prompt ('Agora o segundo numero'));
+// let n3 = parseInt(prompt ('E o terceiro'));
+// function media(pn1, pn2, pn3){
+//     return ((pn1+pn2+pn3)/3);
+// }
+// let resultado = media(n1,n2,n3);
+// alert (resultado);
+
+// let n1 = parseInt(prompt ('Insira um número'));
+// let n2 = parseInt(prompt ('Insira mais um'));
+// function maior(pn1, pn2){
+//     return Math.max(pn1, pn2);
+// }
+// let numMaior = maior(n1, n2);
+// alert (numMaior);
+//
+//OU DESSA FORMA:
+//
+// function maior(pn1, pn2){
+//  if
+
+// let num = parseInt(prompt('Coloque aqui um número'));
+// function multiplicação (n){
+//     return (n*n);
+// }
+// let mult = multiplicação(num);
+// alert (mult);
---
+++ exer.js
@@ -0,0 +1,76 @@
+//Questões dia 11/04/2024
+
+// let alt = parseFloat(prompt("Digite sua altura (em metros)"));
+// let peso = parseFloat(prompt("Digite seu peso (em kg)"));
+// function dados (parAlt, parPeso){
+//      return (parPeso / (parAlt*parAlt));
+// }
+// let imc = dados (alt, peso);
+// alert (`O IMC é de ${imc.toFixed(2)}`);
+
+
+// let num = prompt ('digite um numero');
+// let fatorial = 1;
+// function calFatorial (parNumero){
+//     if(parNumero == 1 || parNumero == 0){
+//        return 1
+//     }else{
+//         numero = 1
+//         for(let i = num1; i>1 ; i-- ){
+//             numero *= i;
+//         }
+//     return numero
+//     }  
+   
+   
+// }
+// let fatorial = fatorial1(num);
+// alert (`A fatorial desse numero é ${fatorial}`);
+
+// let din = parseFloat(prompt('Digite o valor que deseja converter'));
+// function conversor (reais){
+//     return (reais*4.80)
+// }
+// let convertido = conversor(din);
+// alert (`O valor convertido é de ${convertido.toFixed(2)}`);
+
+
+// let lado1 = parseInt(prompt ('digite o valor de um lado'));
+// let lado2 = parseInt(prompt ('qual o valor da base?'));
+// let lado3 = parseInt(prompt ('E da outra base?'));
+// let lado4 = parseInt(prompt ('digite o valor de outro lado'));
+// function are (ladoA,ladoB){
+//     return (ladoA*ladoB);
+// }
+// let area = are (lado1, lado2);
+// function perim (lado5,lado6,lado7,lado8){
+//     return (lado5+lado6+lado7+lado8);
+// }
+// let perimetro = perim (lado1,lado2,lado3,lado4);
+// alert(`A area da sala é ${area} e o seu perimetro é ${perimetro}`);
+
+
+// let raio = prompt ('Qual a medida do raio da sala?');
+// function perimetrou (raio1){
+//     return (2*(3.14*raio1));
+// }
+// let perim = perimetrou(raio);
+ 
+// function are (raio2){
+//     return (3.14*raio2^2);
+// }
+// let area = are (raio);
+// alert(`O perimetro da sala é ${perim.toFixed(2)} e sua Area é ${area}`);
+
+
+// let num = parseInt(prompt("Digite o valor do número para saber sua tabuada:"));
+// let i = 1
+// let resultado;
+// function tabuada(n){
+//     while(i<=10){
+//     resultado = n*i
+//     alert(`${n}x${i}=${resultado}.`);
+//     i++
+//     }
+// }
+// tabuada(num);
